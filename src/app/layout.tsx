b5ba2716923230dc4e2ff944b8ae437361dad129
Angel Roman <angel427roman@gmail.com>
2025-02-10 13:36:24 -0500

    refactor(theme): changed default theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} ${oswald.variable} antialiased dark:bg-black`}>
         <Analytics />
         <SpeedInsights />
-        <ThemeProvider attribute="class" enableSystem disableTransitionOnChange defaultTheme="light">
+        <ThemeProvider attribute="class" enableSystem disableTransitionOnChange defaultTheme="dark">
           <Navbar />
           {children}
           <ContactSidebar />
